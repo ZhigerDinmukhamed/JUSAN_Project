41b964b420d063a29600af8fd27d7acccb5c437a
Zhiga <dinmukhamedzhiger@gmail.com>
2025-06-19 12:20:22 +0500

    add new classes and interfaces

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,18 +1,34 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long JusanId;
+    @Column(name = "iin", length = 255)
+    private String IIN;
+    @Column(name = "full_name", length = 255)
+    private String FullName;
+    @Column(name = "first_name", length = 255)
+    private String FirstName;
+    @Column(name = "last_name", length = 255)
+    private String LastName;
+    @Column(name = "client_type", length = 255)
+    private String ClientType;
+    @Column(name = "client_code", length = 255)
+    private String ClientCode;
+    @Column(name = "doctype", length = 255)
+    private String Doctype;
+    @Column(name = "file_name", length = 255)
+    private String FileName;
+    @Column(name = "file_bytes", length = 255)
+    private String FileBytes;
+    @Column(name = "user_code", length = 255)
+    private String UserCode;
 
-    private String username;
-    private String email;
 }
